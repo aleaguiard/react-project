ad4015f714bddb9baec4e5183197bdcad391d7f9
Ale Aguiar Díaz <122371223+aleaguiard@users.noreply.github.com>
2024-03-26 16:27:16 +0000

    Actualización automática de la hora

--- proyecto-react/src/App.tsx
+++ proyecto-react/src/App.tsx
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const Home = () => <h1>Home</h1>;
-const DateComponent = () => <h1>{new Date().toLocaleString()}</h1>;
+
+const DateComponent = () => {
+  const [date, setDate] = useState(new Date().toLocaleDateString());
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  useEffect(() => {
+    const updateDate = () => {
+      setDate(new Date().toLocaleDateString());
+    };
+    const intervalId = setInterval(updateDate, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  useEffect(() => {
+    const updateTime = () => {
+      setTime(new Date().toLocaleTimeString());
+    };
+    const intervalId = setInterval(updateTime, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div>
+      <h1>Fecha: {date}</h1>
+      <h1>Hora: {time}</h1>
+    </div>
+  );
+};
 
 const App: React.FC = () => {
   return (
