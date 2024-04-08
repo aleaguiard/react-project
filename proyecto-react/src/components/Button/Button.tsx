ad448aa578545af8f0c6acfc6e90e50da42a5223
Ale Aguiar Díaz <122371223+aleaguiard@users.noreply.github.com>
2024-04-08 15:58:21 +0100

    Refactorización boton

--- proyecto-react/src/components/Button/Button.tsx
+++ proyecto-react/src/components/Button/Button.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import './Button.css';
+import { NavLink } from 'react-router-dom';
 
 interface ButtonProps {
     onClick?: () => void;
+    to?: string;
+    href?: string;
     children: React.ReactNode;
 }
 
-const Button: React.FC<ButtonProps> = ({ onClick, children }) => {
+const Button: React.FC<ButtonProps> = ({ onClick, to, href, children }) => {
+    if (to && !href) {
+        return (
+            <NavLink to={to} className="button">
+                {children}
+            </NavLink>
+        );
+    }
+
+    if (href && !to) {
+        return (
+            <a href={href} className="button">
+                {children}
+            </a>
+        );
+    }
+
     return (
         <button className="button" onClick={onClick}>
             {children}
